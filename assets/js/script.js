4fc4030e52012b57a6ce79da7c7209dea891673e
Sander Stolk <ssstolk@gmail.com>
2026-06-29 16:06:49 +0200

    Clean up Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,46 @@
-// Place holder thing
-console.log("EMERGENCE website loaded");
+/* === NAVBAR INSERTION === */
+const navbarElement = document.getElementById('navbar-placeholder');
+if (navbarElement) {
+  fetch('assets/sections/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      navbarElement.innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Failed to load navbar:', error);
+  });
+}
+
+/* === FOOTER INSERTION === */
+
+const footerElement = document.getElementById('footer-placeholder');
+if (footerElement) {
+  fetch('assets/sections/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        // Add footer
+        footerElement.innerHTML = data;
+        // In case we are already on the updates page, remove footer-div-updates
+        const divUpdates = document.getElementById("footer-div-updates");
+        if (divUpdates && window.location.href.includes("/updates")) {
+            divUpdates.outerHTML = "";
+        }
+    })
+    .catch(error => {
+        console.error('Failed to load footer:', error);
+    });
+}
+
+
+/* === QUALTRIX-IFRAME AND RESIZING === */
+
+// Auto-resize Qualtrics iframe
+const iframeElement = document.getElementById('qualtrics-iframe');
+if (iframeElement) {
+  // Also listen for messages from Qualtrics (better method)
+  window.addEventListener('message', function(event) {
+      if (event.origin.includes('qualtrics.com') && event?.data?.height) {
+      iframeElement.style.height = event.data.height + 'px';
+      }
+  });
+}
